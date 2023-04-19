5225a149246388210da621ed53f0ebf73159e064
semlex <alexpskov60@gmail.com>
2023-04-19 17:54:59 +0300

    frontend warning smlfix

--- OCRScan.Frontend/src/pages/index.tsx
+++ OCRScan.Frontend/src/pages/index.tsx
@@ -1,12 +1,8 @@
-import { Inter } from 'next/font/google'
-import { Box, Container, Link, Typography } from '@mui/material'
 import dynamic from 'next/dynamic'
 
 const ImgToPdf = dynamic(() => import('../components/screens/img-to-pdf'), {
   ssr: false,
 })
-
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return <ImgToPdf />
